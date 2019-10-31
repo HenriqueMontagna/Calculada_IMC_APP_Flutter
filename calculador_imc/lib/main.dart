565614a0ac1b0164b48f16f63a8bdd7f107b3a8f
Henrique La Montagna <henriquelamontagna@gmail.com>
2019-10-31 17:37:44 -0300

    App pronto, apk e icone add

--- calculador_imc/lib/main.dart
+++ calculador_imc/lib/main.dart
@@ -16,10 +16,15 @@ class _HomeState extends State<Home> {
 
   String _textoDeInformacao = "Informe Seus Dados";
 
+  GlobalKey<FormState> _chaveFormulario = GlobalKey<FormState>();
+
   void _resetCampos() {
-    _alturaController.text = "";
-    _pesoController.text = "";
-    _textoDeInformacao = "Informe Seus Dados";
+    setState(() {
+      _alturaController.text = "";
+      _pesoController.text = "";
+      _textoDeInformacao = "Informe Seus Dados";
+      _chaveFormulario = GlobalKey<FormState>();
+    });
   }
 
   void _calculaIMC() {
@@ -64,61 +69,77 @@ class _HomeState extends State<Home> {
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(
-              Icons.person_outline,
-              color: Colors.green,
-              size: 120,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Peso(Kg)",
-                  labelStyle: TextStyle(color: Colors.green)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25.0),
-              controller: _pesoController,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Altura(cm)",
-                  labelStyle: TextStyle(color: Colors.green)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25.0),
-              controller: _alturaController,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-              child: Container(
-                height: 50.0,
-                width: 10.0,
-                child: RaisedButton(
-                  onPressed: _calculaIMC,
-                  child: Text(
-                    "Calcular",
-                    style: TextStyle(fontSize: 25.0),
-                  ),
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Form(
+            key: _chaveFormulario,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(
+                  Icons.person_outline,
                   color: Colors.green,
-                  textColor: Colors.white,
+                  size: 120,
                 ),
-              ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Peso(Kg)",
+                      labelStyle: TextStyle(color: Colors.green)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                  controller: _pesoController,
+                  validator: (valor) {
+                    if (valor.isEmpty) {
+                      return "Insira seu Peso!";
+                    }
+                  },
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Altura(cm)",
+                      labelStyle: TextStyle(color: Colors.green)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                  controller: _alturaController,
+                  validator: (valor) {
+                    if(valor.isEmpty){
+                      return "Insira sua Altura!";
+                    }
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Container(
+                    height: 50.0,
+                    width: 10.0,
+                    child: RaisedButton(
+                      onPressed: (){
+                        if(_chaveFormulario.currentState.validate()){
+                          _calculaIMC();
+                        }
+                      },
+                      child: Text(
+                        "Calcular",
+                        style: TextStyle(fontSize: 25.0),
+                      ),
+                      color: Colors.green,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: 0.0, top: 30.0, bottom: 0.0, right: 0.0),
+                  child: Text(
+                    _textoDeInformacao,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 30.0),
+                  ),
+                )
+              ],
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                  left: 0.0, top: 30.0, bottom: 0.0, right: 0.0),
-              child: Text(
-                _textoDeInformacao,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 30.0),
-              ),
-            )
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
